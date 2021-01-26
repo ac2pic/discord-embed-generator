d5b8d7bf91b99f72469497c96bde42c84924d727
ac2pic <ac2pic@gmail.com>
2021-01-26 13:57:04 -0600

    Testing redirects

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const express = require('express'),
 	bodyParser = require('body-parser'),
 	he = require('he'),
 	port = process.env.PORT || 3123;
-const URL = '';
-
-
 
 app.use(bodyParser.json());
 
@@ -13,7 +10,7 @@ app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
 	next();
-  });
+});
 
 app.get('/ping', (req, res, next) => {
 	console.log('Pinged server to keep it alive.');
@@ -21,43 +18,10 @@ app.get('/ping', (req, res, next) => {
 });
 
 
-
-function generateEmbed(embed) {
-
-	let title = 'Basic Title',
-		metaTitle = 'Basic Title',
-		metaDescription = '',
-		metaImage = '',
-		metaUrl = 'https://discord-embed-generator.herokuapp.com/',
-		metaColor = '#00FFFF';
-
-	if (embed.message)  metaDescription = embed.message;
-	if (embed.image) metaImage = embed.image;
-
-		return `<!DOCTYPE html>
-				<html>
-					<head>
-						<title>${title}</title>
-						<meta content="${metaTitle}" property="og:title">
-						<meta property="og:type" content="website">
-						<meta content="${metaUrl}" property="og:url">
-						<meta content="${metaColor}" data-react-helmet="true" name="theme-color"> 
-						<meta content="${metaDescription}" property="og:description">
-						<meta content="${metaImage}" property="og:image">
-					</head>
-				</html>`;
-}
-
 app.get("/embed", (req, res) => {
 	const {title, message, image, hexColor} = req.query;
 
-	res.send(generateEmbed({
-	  title,
-	  message,
-	  image,
-	  URL,
-	  hexColor
-	}));
+	res.redirect('https://twitter.com/search?q='+ req.query.message)''
 });
 
 app.listen(port, function () {
